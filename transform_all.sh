800b8d97887f332cc1b15e05fe19b6072f7bdcc7
Zane Selvans <zane.selvans@catalyst.coop>
2025-08-06 14:03:08 -0600

    Uncomment tables in transform_all.sh

--- transform_all.sh
+++ transform_all.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # Run all the draft transforms.
 # Assumes Dagster extraction has run already.
-#for table in "ferc_cid" "index_pub" "ident" "contracts" "transactions"; do
-for table in "ferc_cid" "index_pub"; do
+for table in "ferc_cid" "index_pub" "ident" "contracts" "transactions"; do
     echo "Transforming $table..."
-    duckdb < "sql/$table.sql"
+    duckdb <"sql/$table.sql"
 done
