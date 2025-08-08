586c2b737961ddbe2808d76488e250f8ccf35659
Zane Selvans <zane.selvans@catalyst.coop>
2025-08-08 09:20:11 -0600

    Update transform_all.sh
    Co-authored-by: Zach Schira <zach.schira@catalyst.coop>

--- transform_all.sh
+++ transform_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run all the draft transforms.
 # Assumes Dagster extraction has run already.
-for table in "ferc_cid" "index_pub" "ident" "contracts" "transactions"; do
+for table in "ferc_company_identifiers" "index_pub" "ident" "contracts" "transactions"; do
     echo "Transforming $table..."
     duckdb <"sql/$table.sql"
 done
